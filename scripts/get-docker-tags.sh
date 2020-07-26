2fd9162ea7df4a305117ea7faa67ef408e442456
Michael Mok <pmmmwh@gmail.com>
2020-07-27 01:26:33 +0800

    chore: format shell scripts

--- scripts/get-docker-tags.sh
+++ scripts/get-docker-tags.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 docker-tags() {
-    local image="$1"
+  local image="$1"
 
-    # Docker v2 API endpoints
-    tokenUri="https://auth.docker.io/token"
-    listUri="https://registry-1.docker.io/v2/$image/tags/list"
+  # Docker v2 API endpoints
+  tokenUri="https://auth.docker.io/token"
+  listUri="https://registry-1.docker.io/v2/$image/tags/list"
 
-    # Get authorization token from the registry
-    data=("service=registry.docker.io" "scope=repository:$image:pull")
-    token="$(curl -s -G --data-urlencode ${data[0]} --data-urlencode ${data[1]} ${tokenUri} | jq -r '.token')"
+  # Get authorization token from the registry
+  data=("service=registry.docker.io" "scope=repository:$image:pull")
+  token="$(curl -s -G --data-urlencode ${data[0]} --data-urlencode ${data[1]} ${tokenUri} | jq -r '.token')"
 
-    # Get list of image tags
-    result="$(curl -s -G -H "Accept: application/json" -H "Authorization: Bearer $token" ${listUri} | jq -r '.tags | @sh')"
+  # Get list of image tags
+  result="$(curl -s -G -H "Accept: application/json" -H "Authorization: Bearer $token" ${listUri} | jq -r '.tags | @sh')"
 
-    echo ${result}
+  echo ${result}
 }
 
 # Ensure the directory containing the output file exists
